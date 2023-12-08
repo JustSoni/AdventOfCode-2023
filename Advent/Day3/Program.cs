67d18db6377f7e6a1d96108e06636938ac52fe5e
Soner Kochan <sonibestbg@gmail.com>
2023-12-08 21:21:51 +0200

    Day 4, Task 1

--- Advent/Day3/Program.cs
+++ Advent/Day3/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filePath = @"..\..\..\input.txt";
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    string inputLine = sr.ReadLine();
+                }
+            }
         }
     }
 }
--- Advent/Day4/Program.cs
+++ Advent/Day4/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filePath = @"..\..\..\input.txt";
+
+            int totalPoints = 0;
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                int current = 0;
+                while (sr.Peek() >= 0)
+                {
+                    current = 0;
+                    string inputLine = sr.ReadLine();
+                    string[] split = inputLine.Split(": ");
+
+                    string[] halves = split[1].Split(" | ");
+
+                    int[] myNumbers = System.Text.RegularExpressions.Regex.Split(halves[0].Trim(), @"\s+").Select(int.Parse).ToArray();
+                    int[] winningNumbers = System.Text.RegularExpressions.Regex.Split(halves[1].Trim(), @"\s+").Select(int.Parse).ToArray();
+
+                    Console.WriteLine(halves[0]);
+                    Console.WriteLine(halves[1]);
+
+                    foreach (var number in myNumbers)
+                    {
+                        if (winningNumbers.Contains(number))
+                        {
+                            if (current == 0)
+                                current = 1;
+                            else
+                                current *= 2;
+                        }
+                    }
+
+
+                    totalPoints += current;
+
+                }
+
+            }
+            Console.WriteLine($"Total points: {totalPoints}");
         }
     }
 }
